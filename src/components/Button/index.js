1ad46d76c3ca70f89f5f2005630e3640c0f6e8d2
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2019-05-09 22:27:10 +0200

    Added button and handleClick function

---
+++ src/components/Button/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Button extends Component {
+
+    render() {
+        
+    return (
+      <div>
+            <button onClick={this.props.onClick}>{this.props.text}</button>
+      </div>
+    )
+  }
+}
+
+export default Button
--- src/components/Joke/index.js
+++ src/components/Joke/index.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import axios from 'axios';
-
+import Button from '../Button';
 export class Joke extends Component {
     state = {
         joke: ''
     }
 
-    componentDidMount() {
+    handleClick = () => {
         axios.get('https://api.chucknorris.io/jokes/random')
             .then(res => this.setState({
                 joke: res.data
             }))
             .catch(err => console.log(err));
+    };
+
+    componentDidMount() {
+        this.handleClick()
     }
 
   render() {
@@ -21,6 +25,7 @@ export class Joke extends Component {
     return (
       <div>
         <h3>{joke.value}</h3>
+        <Button text="New joke" onClick={() => this.handleClick()} />
       </div>
     )
   }
